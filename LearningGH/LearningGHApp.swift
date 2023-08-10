5f149e8399bd52042430fab3701de31fc8e05fa0
mark <mark.karall@gmail.com>
2023-08-10 17:38:17 +0200

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by macbook on 10.08.23.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
